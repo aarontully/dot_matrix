d311e7b5cbcd5b379042f62e5c4fd3b5302a629c
Aaron Tully <aarontully0@gmail.com>
2026-05-18 11:39:41 +1000

    refactor: remove test files and update project metadata to version 1.0.0-alpha

--- test_event_methods.dart
+++
@@ -1,4 +0,0 @@
-import 'package:matrix/matrix.dart';
-void main() {
-  print(MatrixFile(bytes: [], name: 'test.jpg').bytes);
-}
--- test_message_types.dart
+++
@@ -1,5 +0,0 @@
-import 'package:matrix/matrix.dart';
-void main() {
-  print(MessageTypes.Image);
-  print(MessageTypes.Video);
-}
